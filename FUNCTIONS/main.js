088aeae4f3eea9086f567f611d57a66c05ad6ad6
Timuruch <61650380+Timuruch@users.noreply.github.com>
2021-01-15 18:09:25 +0200

    Add files via upload

---
+++ FUNCTIONS/main.js
@@ -0,0 +1,16 @@
+let nam = "";
+let name = "";
+
+function sayHi(){
+	alert("Привіт!");
+}
+
+function greeting(nam){
+	alert(`Привіт ${nam}!`);
+}
+
+function showGreeting(name){
+	var nae = document.getElementById("ty");
+	nae.innerHTML = `Привіт ${name}!`;
+}
+	
