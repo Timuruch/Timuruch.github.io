eca8f16902550960b3e7721643679872f6087de9
Timuruch <61650380+Timuruch@users.noreply.github.com>
2021-04-15 12:29:10 +0300

    Add files via upload

---
+++ heno.py
@@ -0,0 +1,5 @@
+
+
+def starter():
+    print("")
+
---
+++ home.py
@@ -0,0 +1,6 @@
+
+print("You don't instaled news mod")
+print("To do it you need to type")
+print("First you need type '.shop'")
+print("Then you need type 'install'")
+print("And type 'news' and mod activation code will downloaded in cmder folder!")
