d5fe47fe673227a146dc7f66a563a9f880648437
Timuruch <timuruch909@gmail.com>
2021-09-03 20:29:28 +0300

    replace

--- bbs/8801.py
+++ bbs/8801.py
@@ -2,4 +2,5 @@ import time
 
 print("Timuruch bbs oficial server")
 print("Here will be my new news abou my git or my future or not future projects")
+print("And i will post some news")
 print("For today none):")
