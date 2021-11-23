3e47febe9ebd585897dbf87f603dba2d7efeea42
Timuruch <timuruch909@gmail.com>
2021-11-23 17:34:27 +0200

    glisted

--- Programs/glist.py
+++ Programs/glist.py
@@ -1,2 +1,2 @@
 print("Snake:First Condition - 2021 - Timuruch Game corp - 001")
-print("Ping: The pong? - 2021 - Timuruch Game Corp = 001")
+print("Ping: The pong? - 2021 - Timuruch Game Corp - 002")
