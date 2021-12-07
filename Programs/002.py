ae3e7d9652b5601caa2d744561ca7d2e7b2fc411
Timuruch <timuruch909@gmail.com>
2021-12-07 18:22:41 +0200

    bug fixed

--- Programs/002.py
+++ Programs/002.py
@@ -5,8 +5,8 @@ while True:
     x = random.randint(1, 100)
     y = random.randint(1, 100)
     value = input(str(x) + " + " + str(y) + " = ")
-    if x+y == int(value):
-        print("You win. Next round!")
-    elif value == "exit":
+    if value == "exit":
         sys.exit()
+    elif x+y == int(value):
+        print("You win. Next round!")
 
